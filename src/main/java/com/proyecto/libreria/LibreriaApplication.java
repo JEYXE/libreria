81eb01dc22050af6a120ba5624614992d2fa5506
Jorge Caldas <jorge.caldas@outlook.com>
2024-05-18 17:04:41 -0500

    consulta prueba a api

--- src/main/java/com/proyecto/libreria/LibreriaApplication.java
+++ src/main/java/com/proyecto/libreria/LibreriaApplication.java
@@ -1,13 +1,23 @@
 package com.proyecto.libreria;
 
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class LibreriaApplication {
+import com.proyecto.libreria.servicios.ConsumoApi;
 
+@SpringBootApplication
+public class LibreriaApplication implements CommandLineRunner {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LibreriaApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi= new ConsumoApi();
+		var json= consumoApi.obtenerDatos("https://gutendex.com/books/?search=dickens%20great");
+		System.out.println(json);
+}
 }
